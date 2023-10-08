c0ed2be6c7f90298b400657979fc65c438da0e07
taijulsir <princetaijul123@gmail.com>
2023-10-08 18:02:16 +0600

    added gallery section

--- src/Pages/Gallery.jsx
+++ src/Pages/Gallery.jsx
@@ -1,9 +1,128 @@
 
-
+import './gallery.css'
 const Gallery = () => {
     return (
-        <div>
-            
+        <div className="container mx-auto">
+            <h1 className="text-7xl font-bold mt-12 text-center text-amber-600 ">Gallery</h1>
+            <p className="text-center text-4xl font-medium mt-2">On</p>
+            <p className="text-lg mt-3 text-center text-[#403F3F]">Our recent project</p>
+
+            {/* wedding events gallery */}
+            <div>
+                <h1 className='text-4xl font-semibold my-10 '>Wedding Events:</h1>
+                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10 px-7 md:px-0'>
+
+                    <div className='image-container '>
+                        <img className='hover-image h-[238px] w-[354px] ' src="https://i.ibb.co/hdvnRt9/wed.jpg" alt="" />
+                    </div>
+
+                    <div className='image-container '>
+                        <img className='hover-image  h-[238px] w-[354px]' src="https://i.ibb.co/YfJzCDs/wedding.jpg" alt="" />
+                    </div>
+
+                    <div className='image-container '>
+                        <img className='hover-image  h-[238px] w-[354px]' src="https://i.ibb.co/brhLS0s/weddding.jpg" alt="" />
+                    </div>
+
+                    <div className='image-container '>
+                        <img className='hover-image  h-[238px] w-[354px]' src="https://i.ibb.co/JCfKCDt/last.jpg" alt="" />
+                    </div>
+
+                    <div className='image-container '>
+                        <img className='hover-image  h-[238px] w-[354px]' src="https://i.ibb.co/fD7sJzQ/5.jpg" alt="" />
+                    </div>
+
+                    <div className='image-container '>
+                        <img className='hover-image  h-[238px] w-[354px]' src="https://i.ibb.co/XJ99bDZ/6.jpg" alt="" />
+                    </div>
+
+                    <div className='image-container '>
+                        <img className='hover-image  h-[238px] w-[354px]' src="https://i.ibb.co/17rwJbK/7.jpg" alt="" />
+                    </div>
+                    <div className='image-container '>
+                        <img className='hover-image  h-[238px] w-[354px]' src="https://i.ibb.co/vcj88tN/8.webp" alt="" />
+                    </div>
+
+                </div>
+            </div>
+
+            {/* anniversary events gallery */}
+            <div>
+                <h1 className='text-4xl font-semibold my-10 '>Anniversary Events:</h1>
+                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10 px-7 md:px-0'>
+
+                    <div className='image-container '>
+                        <img className='hover-image h-[238px] w-[354px] ' src="https://i.ibb.co/j85HSrC/ann-1.jpg" alt="" />
+                    </div>
+
+                    <div className='image-container '>
+                        <img className='hover-image  h-[238px] w-[354px]' src="https://i.ibb.co/7bm99m1/ann-2.jpg" alt="" />
+                    </div>
+
+                    <div className='image-container '>
+                        <img className='hover-image  h-[238px] w-[354px]' src="https://i.ibb.co/8mh0pBn/ann-3.jpg" alt="" />
+                    </div>
+
+                    <div className='image-container '>
+                        <img className='hover-image  h-[238px] w-[354px]' src="https://i.ibb.co/vVsGt67/ann-4.jpg" alt="" />
+                    </div>
+
+                    <div className='image-container '>
+                        <img className='hover-image  h-[238px] w-[354px]' src="https://i.ibb.co/B6RjcB7/annn-3.jpg" alt="" />
+                    </div>
+
+                    <div className='image-container '>
+                        <img className='hover-image  h-[238px] w-[354px]' src="https://i.ibb.co/b6NfnhL/ann6.jpg" alt="" />
+                    </div>
+
+                    <div className='image-container '>
+                        <img className='hover-image  h-[238px] w-[354px]' src="https://i.ibb.co/Kss9pHt/ann-7.jpg" alt="" />
+                    </div>
+                    <div className='image-container '>
+                        <img className='hover-image  h-[238px] w-[354px]' src="https://i.ibb.co/jkQYx9z/ann-8.jpg" alt="" />
+                    </div>
+
+                </div>
+            </div>
+
+            {/* birthday events gallery */}
+            <div>
+                <h1 className='text-4xl font-semibold my-10 '>Birthday Events:</h1>
+                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10 px-7 md:px-0'>
+
+                    <div className='image-container '>
+                        <img className='hover-image h-[238px] w-[354px] ' src="https://i.ibb.co/NpM5d9H/bd-1.jpg" alt="" />
+                    </div>
+
+                    <div className='image-container '>
+                        <img className='hover-image  h-[238px] w-[354px]' src="https://i.ibb.co/b14Ns1M/bd-2.jpg" alt="" />
+                    </div>
+
+                    <div className='image-container '>
+                        <img className='hover-image  h-[238px] w-[354px]' src="https://i.ibb.co/JpP7RmR/bd-3.jpg" alt="" />
+                    </div>
+
+                    <div className='image-container '>
+                        <img className='hover-image  h-[238px] w-[354px]' src="https://i.ibb.co/1b54jVh/bd-4.jpg" alt="" />
+                    </div>
+
+                    <div className='image-container '>
+                        <img className='hover-image  h-[238px] w-[354px]' src="https://i.ibb.co/rftGP74/bd-5.jpg" alt="" />
+                    </div>
+
+                    <div className='image-container '>
+                        <img className='hover-image  h-[238px] w-[354px]' src="https://i.ibb.co/Hn7DhR6/bd-6.jpg" alt="" />
+                    </div>
+
+                    <div className='image-container '>
+                        <img className='hover-image  h-[238px] w-[354px]' src="https://i.ibb.co/4Y0cnfC/bd-7.webp" alt="" />
+                    </div>
+                    <div className='image-container '>
+                        <img className='hover-image  h-[238px] w-[354px]' src="https://i.ibb.co/2nGQY2X/bd-8.jpg" alt="" />
+                    </div>
+
+                </div>
+            </div>
         </div>
     );
 };
