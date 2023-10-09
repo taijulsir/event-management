5dd923eb62746116f0f6f7ee7dd79164b9380069
taijulsir <princetaijul123@gmail.com>
2023-10-09 09:37:36 +0600

    update about us section description

--- src/Pages/AboutUs.jsx
+++ src/Pages/AboutUs.jsx
@@ -7,77 +7,79 @@ const AboutUs = () => {
         <div>
 
 
-        <div className="2xl:container 2xl:mx-auto lg:py-16 lg:px-20 md:py-12 md:px-6 py-9 px-4">
-            <div className="flex flex-col lg:flex-row justify-between gap-8">
-                <div className="w-full lg:w-5/12 flex flex-col justify-center">
-                    <h1 className="text-3xl lg:text-4xl font-bold leading-9 text-gray-800 pb-4">About Us</h1>
-                    <p className="font-normal text-base leading-6 text-gray-600 ">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum.In the first place we have granted to God, and by this our present charter confirmed for us and our heirs forever that the English Church shall be free, and shall have her rights entire, and her liberties inviolate; and we will that it be thus observed; which is apparent from</p>
+            <div className="2xl:container 2xl:mx-auto lg:py-16 lg:px-20 md:py-12 md:px-6 py-9 px-4">
+                <div className="flex flex-col lg:flex-row justify-between gap-8">
+                    <div className="w-full lg:w-5/12 flex flex-col justify-center">
+                        <h1 className="text-3xl lg:text-4xl font-bold leading-9 text-gray-800 pb-4">About Us</h1>
+                        <p className="font-normal text-base leading-6 text-gray-600 ">Our journey in the world of wedding event management began with a shared passion for celebrations and a belief in the power of love stories. As avid enthusiasts of love, romance, and the magic that unfolds during weddings, we decided to channel our creativity and expertise into a mission  to create exceptional weddings that reflect the love and personalities of each couple.Behind every extraordinary wedding is an extraordinary team, and we are immensely proud of the talent we`ve assembled at [Your Wedding Event Management Company]. Our team consists of seasoned event planners, designers, coordinators, and creative minds who are dedicated to ensuring your day is nothing short of perfection. From concept to execution, we work seamlessly to orchestrate every element of your wedding, leaving you free to savor every moment.</p>
+                    </div>
+                    <div className="w-full lg:w-8/12 ">
+                        <img className="w-full h-full" src="https://i.ibb.co/JCfKCDt/last.jpg" alt="A group of People" />
+                    </div>
                 </div>
-                <div className="w-full lg:w-8/12 ">
-                    <img className="w-full h-full" src="https://i.ibb.co/JCfKCDt/last.jpg" alt="A group of People" />
-                </div>
-            </div>
 
-            <div className="flex lg:flex-row flex-col justify-between gap-8 pt-12">
-                <div className="w-full lg:w-5/12 flex flex-col justify-center">
-                    <h1 className="text-3xl lg:text-4xl font-bold leading-9 text-gray-800 pb-4">Our Story</h1>
-                    <p className="font-normal text-base leading-6 text-gray-600 ">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum.In the first place we have granted to God, and by this our present charter confirmed for us and our heirs forever that the English Church shall be free, and shall have her rights entire, and her liberties inviolate; and we will that it be thus observed; which is apparent from</p>
-                </div>
-                <div className="w-full lg:w-8/12 lg:pt-8">
-                    <div className="grid md:grid-cols-4 sm:grid-cols-2 grid-cols-1 lg:gap-4 shadow-lg rounded-md">
-                        <div className="p-4 pb-6 flex justify-center flex-col items-center">
-                            <img className="md:block hidden" src="https://i.ibb.co/FYTKDG6/Rectangle-118-2.png" alt="Alexa featured Img" />
-                            <img className="md:hidden block" src="https://i.ibb.co/zHjXqg4/Rectangle-118.png" alt="Alexa featured Img" />
-                            <p className="font-medium text-xl leading-5 text-gray-800 mt-4">Alexa</p>
-                        </div>
-                        <div className="p-4 pb-6 flex justify-center flex-col items-center">
-                            <img className="md:block hidden" src="https://i.ibb.co/fGmxhVy/Rectangle-119.png" alt="Olivia featured Img" />
-                            <img className="md:hidden block" src="https://i.ibb.co/NrWKJ1M/Rectangle-119.png" alt="Olivia featured Img" />
-                            <p className="font-medium text-xl leading-5 text-gray-800 mt-4">Olivia</p>
-                        </div>
-                        <div className="p-4 pb-6 flex justify-center flex-col items-center">
-                            <img className="md:block hidden" src="https://i.ibb.co/Pc6XVVC/Rectangle-120.png" alt="Liam featued Img" />
-                            <img className="md:hidden block" src="https://i.ibb.co/C5MMBcs/Rectangle-120.png" alt="Liam featued Img" />
-                            <p className="font-medium text-xl leading-5 text-gray-800 mt-4">Liam</p>
-                        </div>
-                        <div className="p-4 pb-6 flex justify-center flex-col items-center">
-                            <img className="md:block hidden" src="https://i.ibb.co/7nSJPXQ/Rectangle-121.png" alt="Elijah featured img" />
-                            <img className="md:hidden block" src="https://i.ibb.co/ThZBWxH/Rectangle-121.png" alt="Elijah featured img" />
-                            <p className="font-medium text-xl leading-5 text-gray-800 mt-4">Elijah</p>
+                <div className="flex lg:flex-row flex-col justify-between gap-8 pt-12">
+                    <div className="w-full lg:w-5/12 flex flex-col justify-center">
+                        <h1 className="text-3xl lg:text-4xl font-bold leading-9 text-gray-800 pb-4">Our Story</h1>
+                        <p className="font-normal text-base leading-6 text-gray-600 ">Our story starts with a chance meeting at a wedding, where we were both struck by the beauty of love in its purest form. The joy, the laughter, the shared promises — it all left an indelible mark on our hearts. We realized that weddings weren`t just events; they were profound celebrations of life, love, and the joining of two souls.
+
+                            Driven by this newfound appreciation, we decided to embark on a journey that would allow us to immerse ourselves in the world of weddings. It was a leap of faith, but our hearts knew that we had found our calling. We enrolled in wedding planning courses, studied design, and honed our organizational skills, all in pursuit of turning our shared dream into reality.</p>
+                    </div>
+                    <div className="w-full lg:w-8/12 lg:pt-8">
+                        <div className="grid md:grid-cols-4 sm:grid-cols-2 grid-cols-1 lg:gap-4 shadow-lg rounded-md">
+                            <div className="p-4 pb-6 flex justify-center flex-col items-center">
+                                <img className="md:block hidden" src="https://i.ibb.co/FYTKDG6/Rectangle-118-2.png" alt="Alexa featured Img" />
+                                <img className="md:hidden block" src="https://i.ibb.co/zHjXqg4/Rectangle-118.png" alt="Alexa featured Img" />
+                                <p className="font-medium text-xl leading-5 text-gray-800 mt-4">Alexa</p>
+                            </div>
+                            <div className="p-4 pb-6 flex justify-center flex-col items-center">
+                                <img className="md:block hidden" src="https://i.ibb.co/fGmxhVy/Rectangle-119.png" alt="Olivia featured Img" />
+                                <img className="md:hidden block" src="https://i.ibb.co/NrWKJ1M/Rectangle-119.png" alt="Olivia featured Img" />
+                                <p className="font-medium text-xl leading-5 text-gray-800 mt-4">Olivia</p>
+                            </div>
+                            <div className="p-4 pb-6 flex justify-center flex-col items-center">
+                                <img className="md:block hidden" src="https://i.ibb.co/Pc6XVVC/Rectangle-120.png" alt="Liam featued Img" />
+                                <img className="md:hidden block" src="https://i.ibb.co/C5MMBcs/Rectangle-120.png" alt="Liam featued Img" />
+                                <p className="font-medium text-xl leading-5 text-gray-800 mt-4">Liam</p>
+                            </div>
+                            <div className="p-4 pb-6 flex justify-center flex-col items-center">
+                                <img className="md:block hidden" src="https://i.ibb.co/7nSJPXQ/Rectangle-121.png" alt="Elijah featured img" />
+                                <img className="md:hidden block" src="https://i.ibb.co/ThZBWxH/Rectangle-121.png" alt="Elijah featured img" />
+                                <p className="font-medium text-xl leading-5 text-gray-800 mt-4">Elijah</p>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div className="flex lg:flex-row flex-col justify-between gap-8 pt-12">
-                <div className="w-full lg:w-5/12 flex flex-col justify-center">
-                    <h1 className="text-3xl lg:text-4xl font-bold leading-9 text-gray-800 pb-4">Our Vision</h1>
-                    <p className="font-normal text-base leading-6 text-gray-600 ">Our wedding is not just a celebration of our love; it`s a reflection of our personalities and the life we`ve built together. We envision a day filled with [adjectives like love, joy, laughter], surrounded by our dearest friends and family.</p>
-                </div>
-                <div className="w-full lg:w-8/12 lg:pt-8">
-                    <div className="grid md:grid-cols-4 sm:grid-cols-2 grid-cols-1 lg:gap-4 shadow-lg rounded-md">
-                        <div className="p-4 pb-6 flex justify-center flex-col items-center">
-                            <img className="md:block hidden h-[200px]" src="https://i.ibb.co/fD7sJzQ/5.jpg" alt="Alexa featured Img" />
-                            <img className="md:hidden block h-[200px]" src="https://i.ibb.co/fD7sJzQ/5.jpg" alt="Alexa featured Img" />
-                            <p className="font-medium text-xl leading-5 text-gray-800 mt-4">John Smith</p>
-                        </div>
-                        <div className="p-4 pb-6 flex justify-center flex-col items-center">
-                            <img className="md:block hidden h-[200px]" src="https://i.ibb.co/XJ99bDZ/6.jpg" alt="Olivia featured Img" />
-                            <img className="md:hidden block h-[200px]" src="https://i.ibb.co/XJ99bDZ/6.jpg" alt="Olivia featured Img" />
-                            <p className="font-medium text-xl leading-5 text-gray-800 mt-4">Olivia Curl</p>
-                        </div>
-                        <div className="p-4 pb-6 flex justify-center flex-col items-center">
-                            <img className="md:block hidden h-[200px]" src="https://i.ibb.co/17rwJbK/7.jpg" alt="Liam featued Img" />
-                            <img className="md:hidden block h-[200px]" src="https://i.ibb.co/17rwJbK/7.jpg" alt="Liam featued Img" />
-                            <p className="font-medium text-xl leading-5 text-gray-800 mt-4">Alexa</p>
-                        </div>
-                        <div className="p-4 pb-6 flex justify-center flex-col items-center">
-                            <img className="md:block hidden h-[200px]" src="https://i.ibb.co/vcj88tN/8.webp" alt="Elijah featured img" />
-                            <img className="md:hidden block h-[200px]" src="https://i.ibb.co/vcj88tN/8.webp" alt="Elijah featured img" />
-                            <p className="font-medium text-xl leading-5 text-gray-800 mt-4">Sustmita Roy</p>
+                <div className="flex lg:flex-row flex-col justify-between gap-8 pt-12">
+                    <div className="w-full lg:w-5/12 flex flex-col justify-center">
+                        <h1 className="text-3xl lg:text-4xl font-bold leading-9 text-gray-800 pb-4">Our Vision</h1>
+                        <p className="font-normal text-base leading-6 text-gray-600 ">Our wedding is not just a celebration of our love; it`s a reflection of our personalities and the life we`ve built together. We envision a day filled with [adjectives like love, joy, laughter], surrounded by our dearest friends and family.Our journey as wedding event planners has been a continuous exploration of the profound connection that love creates. From our very first wedding, we realized that love stories are more than just tales; they are living, breathing narratives that deserve to be celebrated in the most extraordinary way.</p>
+                    </div>
+                    <div className="w-full lg:w-8/12 lg:pt-8">
+                        <div className="grid md:grid-cols-4 sm:grid-cols-2 grid-cols-1 lg:gap-4 shadow-lg rounded-md">
+                            <div className="p-4 pb-6 flex justify-center flex-col items-center">
+                                <img className="md:block hidden h-[200px]" src="https://i.ibb.co/fD7sJzQ/5.jpg" alt="Alexa featured Img" />
+                                <img className="md:hidden block h-[200px]" src="https://i.ibb.co/fD7sJzQ/5.jpg" alt="Alexa featured Img" />
+                                <p className="font-medium text-xl leading-5 text-gray-800 mt-4">John Smith</p>
+                            </div>
+                            <div className="p-4 pb-6 flex justify-center flex-col items-center">
+                                <img className="md:block hidden h-[200px]" src="https://i.ibb.co/XJ99bDZ/6.jpg" alt="Olivia featured Img" />
+                                <img className="md:hidden block h-[200px]" src="https://i.ibb.co/XJ99bDZ/6.jpg" alt="Olivia featured Img" />
+                                <p className="font-medium text-xl leading-5 text-gray-800 mt-4">Olivia Curl</p>
+                            </div>
+                            <div className="p-4 pb-6 flex justify-center flex-col items-center">
+                                <img className="md:block hidden h-[200px]" src="https://i.ibb.co/17rwJbK/7.jpg" alt="Liam featued Img" />
+                                <img className="md:hidden block h-[200px]" src="https://i.ibb.co/17rwJbK/7.jpg" alt="Liam featued Img" />
+                                <p className="font-medium text-xl leading-5 text-gray-800 mt-4">Alexa</p>
+                            </div>
+                            <div className="p-4 pb-6 flex justify-center flex-col items-center">
+                                <img className="md:block hidden h-[200px]" src="https://i.ibb.co/vcj88tN/8.webp" alt="Elijah featured img" />
+                                <img className="md:hidden block h-[200px]" src="https://i.ibb.co/vcj88tN/8.webp" alt="Elijah featured img" />
+                                <p className="font-medium text-xl leading-5 text-gray-800 mt-4">Sustmita Roy</p>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
             </div>
 
             <Footer></Footer>
